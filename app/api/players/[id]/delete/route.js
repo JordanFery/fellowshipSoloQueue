afc53ea6fce5e09f2f48502f2d145e1419370d08
jordanfery <j.fery@hotmail.fr>
2025-11-25 06:17:45 +0100

    deploy fix - delete the delete api route

--- app/api/players/[id]/delete/route.js
+++
@@ -1,8 +0,0 @@
-import { prisma } from "@/lib/prisma";
-
-export async function DELETE(req, { params }) {
-    const id = parseInt(params.id);
-    await prisma.playerRequest.delete({ where: { id } });
-
-    return new Response(JSON.stringify({ success: true }), { status: 200 });
-}
