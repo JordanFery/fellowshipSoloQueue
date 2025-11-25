41e641e4993c973a0e2a6ff1bf05d10dbe525431
jordanfery <j.fery@hotmail.fr>
2025-11-25 06:29:37 +0100

    test modifying lib/prisma

--- app/lib/prisma.js
+++ app/lib/prisma.js
@@ -1,24 +1,38 @@
-import { PrismaClient } from '@prisma/client'
-import { PrismaPg } from '@prisma/adapter-pg'
-import pg from 'pg'
+// import { PrismaClient } from '@prisma/client'
+// import { PrismaPg } from '@prisma/adapter-pg'
+// import pg from 'pg'
 
-const { Pool } = pg
+// const { Pool } = pg
+
+// const globalForPrisma = globalThis
+
+// if (!globalForPrisma.prisma) {
+//     const pool = new Pool({
+//         connectionString: process.env.DATABASE_URL,
+//     })
+
+//     const adapter = new PrismaPg(pool)
+
+//     globalForPrisma.prisma = new PrismaClient({
+//         adapter,
+//         log: process.env.NODE_ENV === 'development' ? ['query', 'error', 'warn'] : ['error'],
+//     })
+// }
+
+// const prisma = globalForPrisma.prisma
+
+// export default prisma
+
+import { PrismaClient } from '@prisma/client'
 
 const globalForPrisma = globalThis
 
-if (!globalForPrisma.prisma) {
-    const pool = new Pool({
-        connectionString: process.env.DATABASE_URL,
+export const prisma =
+    globalForPrisma.prisma ??
+    new PrismaClient({
+        log: process.env.NODE_ENV === 'development' ? ['query'] : ['error'],
     })
 
-    const adapter = new PrismaPg(pool)
-
-    globalForPrisma.prisma = new PrismaClient({
-        adapter,
-        log: process.env.NODE_ENV === 'development' ? ['query', 'error', 'warn'] : ['error'],
-    })
-}
-
-const prisma = globalForPrisma.prisma
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
 
 export default prisma
