8159e25c178c7c50a3529432ab961eceaaa9f022
jordanfery <j.fery@hotmail.fr>
2025-11-25 06:32:27 +0100

    try with url in schema prisma

--- app/lib/prisma.js
+++ app/lib/prisma.js
@@ -23,16 +23,24 @@
 
 // export default prisma
 
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from "@prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+import pg from "pg";
 
-const globalForPrisma = globalThis
+const globalForPrisma = globalThis;
 
-export const prisma =
-    globalForPrisma.prisma ??
-    new PrismaClient({
-        log: process.env.NODE_ENV === 'development' ? ['query'] : ['error'],
-    })
+if (!globalForPrisma.prisma) {
+    const pool = new pg.Pool({
+        connectionString: process.env.DATABASE_URL,
+    });
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
+    const adapter = new PrismaPg(pool);
 
-export default prisma
+    globalForPrisma.prisma = new PrismaClient({
+        adapter,
+    });
+}
+
+const prisma = globalForPrisma.prisma;
+
+export default prisma;
